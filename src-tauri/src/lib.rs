723620c5e62b92b14abd8af1a90f8dc975e44ded
Alysonhower <contato@alysonhower.dev>
2024-11-18 18:03:37 -0300

    cleanup: remove unused deps

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,18 +3,9 @@ use commands::*;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .invoke_handler(tauri::generate_handler![greet])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        // .plugin(tauri_plugin_shell::init()) // uncomment to use shell
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
